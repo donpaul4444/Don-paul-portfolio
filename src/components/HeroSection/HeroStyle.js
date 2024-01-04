00de8378063400b6c3c4047f2a6d5cf685038cf8
donpaul4444 <donpaul4444@gmail.com>
2024-01-04 15:44:18 +0530

    mobile edited

--- src/components/HeroSection/HeroStyle.js
+++ src/components/HeroSection/HeroStyle.js
@@ -96,18 +96,18 @@ export const Img = styled.img`
   position: relative;
   width: 100%;
   height: 100%;
-  max-width: 340px;
+  max-width: 370px;
   max-height: 400px;
   border-radius: 50%;
   border: 2px solid ${({ theme }) => theme.primary};
 
   @media (max-width: 768px) {
-    max-width: 400px;
+    max-width: 370px;
     max-height: 400px;
   }
 
   @media (max-width: 640px) {
-    max-width: 280px;
+    max-width: 250px;
     max-height: 280px;
   }
 `;
